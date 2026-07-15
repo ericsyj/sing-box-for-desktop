7408b9a5e3a96bc33e0b4878e2de1b7b1c40b94e
reF1nd <ref1nd@thunder-cloud.cc>
2026-07-15 17:43:38 +0800

    Add reF1nd desktop release identity

--- src/main/x11Tray.ts
+++ src/main/x11Tray.ts
@@ -39,7 +39,7 @@ function iconPixmaps(iconPath: string): IconPixmap[] {
 
 class StatusNotifierItem extends dbus.interface.Interface {
   readonly Category = "ApplicationStatus";
-  readonly Id = "sing-box";
+  readonly Id = "sing-box-reF1nd";
   readonly Title = "sing-box";
   readonly Status = "Active";
   readonly WindowId = 0;
